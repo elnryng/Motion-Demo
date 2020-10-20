0ced05ff5dc2c5367d14554b2acdc0258b54d1d9
elnryng <63319934+elnryng@users.noreply.github.com>
2020-10-20 15:47:05 -0400

    added finishing touches

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,13 +2,14 @@
 import {gsap} from "gsap";
 
 import {glassFillAnimation} from './glassFill.js';
-import {loadingTextAnimation} from './text.js';
+import {loadingTextAnimation, doneTextAnimation} from './text.js';
 
 
 var mainTL = gsap.timeline();
 
 mainTL.add(glassFillAnimation(), "begin")
         .add(loadingTextAnimation(),"begin")
+        .add(doneTextAnimation())
         
 
 
--- dev/js/text.js
+++ dev/js/text.js
@@ -11,6 +11,25 @@ export function loadingTextAnimation(){
                 scale: 0,
                 rotationX:180 
                 
+            })
+        .to("#loading",
+        {
+            alpha: 0,
+            duration: 0.5
+        })
+                
+    return tl;
+}
+
+export function doneTextAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#done",
+            {
+                alpha:0,
+                duration:  1,
+                scale: 0,
+                rotationX:180 
+                
             });
                 
     return tl;
